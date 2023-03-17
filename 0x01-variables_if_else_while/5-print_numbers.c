62ceae79806dba3e10f5570364d9b13fe69d07cc
mayada <mm0425689@gmail.com>
2023-03-17 16:35:58 -0700

    p

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{         int ch;
+	for (ch = 0; ch <= 10; ch++)
+		printf("%d", ch);
+	putchar('\n');
+	return (0);
+}
