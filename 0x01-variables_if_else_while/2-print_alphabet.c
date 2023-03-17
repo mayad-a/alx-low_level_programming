18b9dffa03648c4d678d7e5c334c67818cf70711
mayada <mm0425689@gmail.com>
2023-03-17 16:05:19 -0700

    p

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0; i < 26; i++)
+		putchar('a' + i);
+	return (0);
+}
