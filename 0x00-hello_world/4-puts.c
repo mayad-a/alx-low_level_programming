25688f37f7c73ffcb5b972c80350e565e60ddaf3
mayada <mm0425689@gmail.com>
2023-03-17 10:23:11 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,1 @@
-#include<stdio.h>
-int main (void)
-{ 
-	char str1[] = "\"Programming is like building a multilingual puzzle";
-	puts(str1) ; 
-	return (0) ;
-}
+#include<stdio.h> int main(void) { puts("\"Programming is like building a multilingual puzzle");  return (0); }
