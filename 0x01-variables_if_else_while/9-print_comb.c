393fd63e6e18ee34761c54304f6c045670b3f925
mayada <mm0425689@gmail.com>
2023-03-20 09:15:52 -0700

    D

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-	char n;
+	int n;
 
-	for (n = '1'; n <= '9'; n++)
+	for (n = 48; n <= 58; n++)
 	{
 		putchar(n);
-		if (n != '9')
+		if (n != 57)
 		{
 			putchar(',');
 			putchar(' ');
