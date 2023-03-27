5d5a21e9b963e9e3e738cdcbb685c52c67adb796
mayada <mm0425689@gmail.com>
2023-03-27 16:49:06 -0700

    w

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * function that takes a pointer
+ *
+ * to an int as parameter and updates the value
+ * it points to to 98
+ */
+void reset_to_98(int *n);
+{
+    int a = 402;
+
+    *n = &a;
+    *n = 98;
+    return (0);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main_h
+
+void reset_to_98(int *n);
+int _putchar(char c);
+#endif
